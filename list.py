f8f107fc9fa7b214f21b912db19ea0381fba9094
Zoe <surui2890@gmail.com>
2018-09-26 22:36:43 -0700

    list

---
+++ list.py
@@ -0,0 +1,10 @@
+rockets_player_1 = 'Rory'
+rockets_player_2 = 'Rav'
+rockets_player_3 = 'Rachel'
+planets_player_1 = 'Peter'
+planets_player_2 = 'Pablo'
+planets_player_3 = 'Polly'
+rockets_playes = ['Rory', 'Rav', 'Rachel', 'Renata', 'Ryan', 'Ruby']
+planets_players = ['Peter', 'Pablo', 'Polly', 'Penny', 'Paula', 'Patrick']
+print(rockets_playes[0])
+print(planets_players[4])
