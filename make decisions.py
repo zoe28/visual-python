4863c95372b9e6e72d9abc7e3512ef2e6ff7efb9
Zoe <surui2890@gmail.com>
2018-09-27 22:36:29 -0700

    examples

--- make decisions.py
+++ make decisions.py
@@ -6,3 +6,8 @@ print(pineapples == zebras)
 print(pineapples != zebras)
 print(pineapples == 4) and (zebras == 2)
 print(pineapples == 5) or (zebras == 1)
+
+age = 10
+height = 60
+print(age > 7) and (height > 46)
+ 
