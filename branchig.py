80b0db781152948c2ce181956d42d093b98f47d2
Zoe <surui2890@gmail.com>
2018-10-04 23:08:39 -0700

    added

--- branchig.py
+++ branchig.py
@@ -1,3 +1,9 @@
-is_dark = input('Is it dark outside? (y/n)')
+is_dark = input('Is it dark outside? (y/n)\n')
 if is_dark == 'y':
   print('GoodNight! Zzzzzzzzzzzzz...')
+
+tentacles = input('Do you have tentacles? (y/n)')
+if tentacles -- 'y':
+  print('I never knew octopuses could type!')
+else:
+      print('Greetingd, human!')  
