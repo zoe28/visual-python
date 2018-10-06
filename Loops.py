cb66c3abf9fcffab0859987e419decd2a1781109
Zoe <surui2890@gmail.com>
2018-10-05 17:11:54 -0700

    infinite loops

--- Loops.py
+++ Loops.py
@@ -7,3 +7,6 @@ while answer == 'y':
   hippos = hippos + 1
   print(str(hippos) + ' balancing hippos!')
   answer = input('Add another hippo? (y/n)')
+
+while True:
+  print('This is an infinite loop!')
