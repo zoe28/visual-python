c5266abc6bd8491da4f34d95d80d7ae17305de7c
Zoe <surui2890@gmail.com>
2018-10-08 22:20:04 -0700

    print beside for loops

--- Loops1.py
+++ Loops1.py
@@ -7,3 +7,8 @@ for hooray_counter in range(1, 3):
   for hip_counter in range(1, 4):
     print('Hip')
     print('Hooray!')
+
+for hooray_counter in range(1, 3):
+  for hip_counter in range(1, 4):
+    print('Hip')
+print('Hooray!')
