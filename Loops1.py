0c71985934df222181a2aca64d9586a3be582472
Zoe <surui2890@gmail.com>
2018-10-08 22:18:58 -0700

    different for loops

--- Loops1.py
+++ Loops1.py
@@ -2,3 +2,8 @@ for hooray_counter in range(1, 3):
   for hip_counter in range(1, 0):
     print('Hip')
   print('Hooray!')
+
+for hooray_counter in range(1, 3):
+  for hip_counter in range(1, 4):
+    print('Hip')
+    print('Hooray!')
