258e4b36218c049942b4bbfd9012b773151db887
Zoe <surui2890@gmail.com>
2018-10-09 18:02:26 -0700

    more questions

--- animaQuiz.py
+++ animaQuiz.py
@@ -7,3 +7,5 @@ score = 0
 print('Guess the Animal!')
 guess1 = input('Which bear lives at the North Pole? ')
 check_guess(guess1, 'polar bear')
+guess2 = input('Which is the fastest land animal? ')
+check_guess(guess2, "cheetah")
