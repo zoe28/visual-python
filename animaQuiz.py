d429e861e8a0ada794beca9879833ee631eaa1dc
Zoe <surui2890@gmail.com>
2018-10-09 18:06:25 -0700

    score calculation

--- animaQuiz.py
+++ animaQuiz.py
@@ -9,3 +9,7 @@ guess1 = input('Which bear lives at the North Pole? ')
 check_guess(guess1, 'polar bear')
 guess2 = input('Which is the fastest land animal? ')
 check_guess(guess2, "cheetah")
+guess3 = input('Which is the largest animal? ')
+check_guess(guess3, 'blue whale')
+
+print('Your score is ' + str(score))
