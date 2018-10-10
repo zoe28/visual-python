862eecaac821e73d0e44c06a7c84eba56fa5370c
Zoe <surui2890@gmail.com>
2018-10-09 17:28:21 -0700

    check function

--- animaQuiz.py
+++ animaQuiz.py
@@ -1,2 +1,8 @@
+def check_guess(guess, answer):
+  global score
+  if guess == answer:
+    print('Correct answer')
+    score = score + 1
 score = 0
 print('Guess the Animal!')
+guess1 = input('Which bear lives at the North Pole? ')
