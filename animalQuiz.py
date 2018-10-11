b893f4cad1c5d82a03a13d90882fe71ba252db67
Zoe <surui2890@gmail.com>
2018-10-10 17:42:07 -0700

    give more times to try

---
+++ animalQuiz.py
@@ -0,0 +1,19 @@
+def check_guess(guess, answer):
+  global score
+  still_guessing = True
+  attempt = 0
+  while still_guessing and attempt < 3:
+    if guess.lower() == answer.lower():
+    #if guess.upper() == answer.upper():
+    print('Correct answer')
+    score = score + 1
+score = 0
+print('Guess the Animal!')
+guess1 = input('Which bear lives at the North Pole? ')
+check_guess(guess1, 'polar bear')
+guess2 = input('Which is the fastest land animal? ')
+check_guess(guess2, "cheetah")
+guess3 = input('Which is the largest animal? ')
+check_guess(guess3, 'blue whale')
+
+print('Your score is ' + str(score))
