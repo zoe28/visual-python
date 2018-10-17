4816ff442aaed4862c791772236d6ef4115f8be9
Zoe <surui2890@gmail.com>
2018-10-16 18:28:01 -0700

    another way of calling

--- function.py
+++ function.py
@@ -2,5 +2,10 @@ name = input('What\'s your name')
 greeting = 'Hello ' + name
 print(greeting)
 
-max(10, 23, 34, 45, 643)
-print(max)
+#max(10, 23, 34, 45, 643)
+#print(max)
+#min(12, 343, 565, 3, 65)
+
+message = 'Python makes me happy?'
+message.replace('happy', ':D')
+print(message.replace)
