ac718733ac13edbf055b29eb5ae392e7b837df5b
Zoe <surui2890@gmail.com>
2018-10-17 18:12:04 -0700

    added parameters

--- function1.py
+++ function1.py
@@ -6,3 +6,10 @@ def print_seconds_per_day():
 
 print_seconds_per_day()
 
+def print_seconds_per_day(days):
+  hours = days * 24
+  minutes = hours * 60
+  seconds = minutes * 60
+  print(seconds)
+
+print_seconds_per_day(7)
