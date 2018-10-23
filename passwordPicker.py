d4cd0f4513c7a6f84562f8da6f1267b5269ec44f
Zoe <surui2890@gmail.com>
2018-10-22 18:28:47 -0700

    indentation errors

--- passwordPicker.py
+++ passwordPicker.py
@@ -6,7 +6,10 @@ nouns = ['apple', 'name', 'coffee', 'San Franciso', 'wine', 'hammer', 'panada', 
 
 print('Welcome to Password Picker!')
 
-  adjectives = random.choice(adjectives)
-  nouns = random.choice(nouns)
-  number = random.randrange(0, 100)
-  special_char = random.choice(string.punctuation)
+adjectives = random.choice(adjectives)
+nouns = random.choice(nouns)
+number = random.randrange(0, 100)
+special_char = random.choice(string.punctuation)
+
+password = adjectives + nouns + str(number) + special_char
+print('Your new password is: ')
