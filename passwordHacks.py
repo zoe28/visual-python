b03d29134976b71b4e08afc60a6a86e5e9914352
Zoe <surui2890@gmail.com>
2018-10-25 19:49:01 -0700

    get multiple passwords

--- passwordHacks.py
+++ passwordHacks.py
@@ -5,3 +5,15 @@ adjectives = ['sleepy', 'happy', 'slow', 'sad', 'wet', 'strong', 'fat', 'yellow'
 nouns = ['apple', 'name', 'coffee', 'San Franciso', 'wine', 'hammer', 'panada', 'Holiday', 'toaster', 'telephone', 'teacher', 'student', 'hotpot']
 
 print('Welcome to Password Picker!')
+
+while True:
+   
+   for num in range(3):
+     adjectives = random.choice(adjectives)
+     nouns = random.choice(nouns)
+     number = random.randrange(0, 100)
+     special_char = random.choice(string.punctuation)
+
+     password = adjectives + nouns + str(number) + special_char
+     print('Your new password is: {}'.format(password))
+response = input('Would you like more passwords? Type y or n: ')
