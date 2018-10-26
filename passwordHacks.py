42d64125f22408ba217e0ad1ec7b06ba1c0c76ef
Zoe <surui2890@gmail.com>
2018-10-25 19:41:01 -0700

    hacks and tweaks

---
+++ passwordHacks.py
@@ -0,0 +1,7 @@
+import random
+import string
+
+adjectives = ['sleepy', 'happy', 'slow', 'sad', 'wet', 'strong', 'fat', 'yellow', 'fluffy', 'white', 'pround', 'brave']
+nouns = ['apple', 'name', 'coffee', 'San Franciso', 'wine', 'hammer', 'panada', 'Holiday', 'toaster', 'telephone', 'teacher', 'student', 'hotpot']
+
+print('Welcome to Password Picker!')
