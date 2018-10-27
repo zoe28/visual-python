4724eb8c85597544ebc3b903b8dba2cfb01d1c3e
Zoe <surui2890@gmail.com>
2018-10-26 19:27:52 -0700

    remember the result

--- nineLives.py
+++ nineLives.py
@@ -5,4 +5,5 @@ words = ['pizza', 'family', 'teeth', 'otter', 'train', 'plane', 'earth']
 secret_word = random.choice(words)
 clue = list('??????') # store the clue
 heart_symbol = u'\u2764' # show the lives left
-guessed_word_correctly = False
+guessed_word_correctly = False 
+# The variable is set as False to begin with because the player doesn't know the word when the game starts
