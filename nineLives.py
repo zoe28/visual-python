57b9a3c78a6535731eddba6b98423823e50adb76
Zoe <surui2890@gmail.com>
2018-10-26 19:20:53 -0700

    store the clue and show the left lives

--- nineLives.py
+++ nineLives.py
@@ -3,3 +3,6 @@ import random
 lives = 9
 words = ['pizza', 'family', 'teeth', 'otter', 'train', 'plane', 'earth']
 secret_word = random.choice(words)
+clue = list('??????') # store the clue
+heart_symbol = u'\u2764' # show the lives left
+guessed_word_correctly = False
