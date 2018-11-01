7a9d4f42bb25e65fde5742645fbac9dedb438060
Zoe <surui2890@gmail.com>
2018-10-31 18:39:03 -0700

    final step

--- nineLives.py
+++ nineLives.py
@@ -30,3 +30,8 @@ while lives > 0:
   else:
     print('Incorrect. You lose a life')
     lives = lives - 1
+
+if guessed_word_correctly:
+  print('You won! The secret word was ' + secret_word)
+else:
+  print('You lost! The secret word was ' + secret_word)
