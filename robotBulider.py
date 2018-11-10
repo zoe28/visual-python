e5a114b897333b7b77b3725440496985a4c6bb20
Zoe <surui2890@gmail.com>
2018-11-09 21:59:46 -0800

    using rang makes the loop run twice

--- robotBulider.py
+++ robotBulider.py
@@ -6,3 +6,9 @@ def rectangle(horizontal, vertical, color):
   t.color(color)
   t.begin_fill()
   for counter in range(1, 3):
+    t.forward(horizontal)
+    t.right(90)
+    t.forward(vertical)
+    t.right(90)
+  t.end_fill()
+  t.penup()
